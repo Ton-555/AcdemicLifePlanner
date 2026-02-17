a2a27c28d2558ccfef3045a9b431307130060760
Boonyalit356 <boonyalit.g@ku.th>
2026-02-17 11:35:14 +0700

    Fix file app.js

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Test! Academic Life Planner</Text>
-      <StatusBar style="auto" />
-    </View>
+import Dashboard from "./src/screen/Dashboard";
+import Timetable from "./src/screen/Timetable";
+import Planner from "./src/screen/Planner";
+import Profile from "./src/screen/Profile";
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+  return ( 
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen
+          name="Dashboard"
+          component={Dashboard}
+          options={{ title: "หน้าหลัก" }}
+        />
+        <Tab.Screen
+          name="Timetable"
+          component={Timetable}
+          options={{ title: "ตาราง" }}
+        />
+        <Tab.Screen
+          name="Planner"
+          component={Planner}
+          options={{ title: "วางแผน" }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{ title: "โปรไฟล์" }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
